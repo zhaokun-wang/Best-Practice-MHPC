27bee41f2012c6e8923fb35425708f6c15d574b1
zhaokun-wang <ciaran.zhaokun.wang@gmail.com>
2025-12-01 15:23:20 +0100

    doxygen module_physics.f90 update

--- source/module_physics.f90
+++ source/module_physics.f90
@@ -29,6 +29,12 @@ module module_physics
 
   contains
 
+  !> @brief initialize division of the grid, time, and computing all the data which is needed
+  !! for further calculation
+  !!
+  !! @param[out]      etime                 overall runtime
+  !! @param[out]      output_counter        times of output
+  !! @param[out]      dt                    time step of evolve
   subroutine init(etime,output_counter,dt)
     implicit none
     real(wp), intent(out) :: etime, output_counter, dt
@@ -98,6 +104,15 @@ module module_physics
     write(stdout,*) 'MODEL STATUS INITIALIZED.'
   end subroutine init
 
+  !> @brief applying a rungekutta method to evolve 
+  !! in three difference time step and two different directions, 
+  !! each time switch the turn of the calculation on two directions
+  !!
+  !! @param[inout]    s0                    old status
+  !! @param[inout]    s1                    new status
+  !! @param[inout]    fl                    flux
+  !! @param[inout]    tend                  partial derivative of parameters
+  !! @param[in]       dt                    time step of evolve
   subroutine rungekutta(s0,s1,fl,tend,dt)
     implicit none
     type(atmospheric_state), intent(inout) :: s0
@@ -131,6 +146,16 @@ module module_physics
 
   ! Semi-discretized step in time:
   ! s2 = s0 + dt * rhs(s1)
+
+  !> @brief calculate the derivative on given direction 
+  !!
+  !! @param[in]       s0                    old status
+  !! @param[inout]    s1                    new status
+  !! @param[inout]    s2                    status to store the updated result
+  !! @param[inout]    fl                    flux
+  !! @param[inout]    tend                  partial derivative of parameters
+  !! @param[in]       dt                    time step of evolve
+  !! @param[in]       dir                   direction to update
   subroutine step(s0, s1, s2, dt, dir, fl, tend)
     implicit none
     type(atmospheric_state), intent(in) :: s0
@@ -148,6 +173,16 @@ module module_physics
     call s2%update(s0,tend,dt)
   end subroutine step
 
+  !> @brief calculate the constants and perturbations for the given position
+  !!
+  !! @param[in]       x                     x position
+  !! @param[in]       z                     z position
+  !! @param[out]      r                     perturbation term 1
+  !! @param[out]      u                     perturbation term 2
+  !! @param[out]      w                     perturbation term 3
+  !! @param[out]      t                     perturbation term 4
+  !! @param[out]      hr                    constant density for the given height z
+  !! @param[out]      ht                    constant temprature for the given height z
   subroutine thermal(x,z,r,u,w,t,hr,ht)
     implicit none
     real(wp), intent(in) :: x, z
@@ -161,6 +196,11 @@ module module_physics
     t = t + ellipse(x,z,3.0_wp,hxlen,p1,p1,p1)
   end subroutine thermal
 
+  !> @brief calculate the constants for the given height
+  !!
+  !! @param[in]       z                     z position
+  !! @param[out]      r                     constant density for the given height z
+  !! @param[out]      t                     constant temprature for the given height z  
   subroutine hydrostatic_const_theta(z,r,t)
     implicit none
     real(wp), intent(in) :: z
@@ -173,6 +213,15 @@ module module_physics
     r = rt / t
   end subroutine hydrostatic_const_theta
 
+  !> @brief calculate the perturbations for the given position
+  !!
+  !! @param[in]       x                     x position
+  !! @param[in]       z                     z position
+  !! @param[in]       amp                   amplitude
+  !! @param[in]       x0                    x position of the center of the ellipse to calculate the distance from for generating perturbation
+  !! @param[in]       z0                    z position of the center of the ellipse to calculate the distance from for generating perturbation
+  !! @param[in]       x1                    semi axis length 1 for the ellipse
+  !! @param[in]       z1                    semi axis length 2 for the ellipse
   elemental function ellipse(x,z,amp,x0,z0,x1,z1) result(val)
     implicit none
     real(wp), intent(in) :: x, z
@@ -189,6 +238,7 @@ module module_physics
     end if
   end function ellipse
 
+  !> @brief call the delete function for all the used types
   subroutine finalize()
     implicit none
     call oldstat%del_state( )
@@ -198,6 +248,10 @@ module module_physics
     call ref%del_ref( )
   end subroutine finalize
 
+  !> @brief calculate the total mass and energy for the whole grid
+  !!
+  !! @param[out]      mass                  mass
+  !! @param[out]      te                    energy
   subroutine total_mass_energy(mass,te)
     implicit none
     real(wp), intent(out) :: mass, te
